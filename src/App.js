25e37fb78c2998c113824a1310c8ae035baef891
Felix <34609664+z-felixhan@users.noreply.github.com>
2021-11-07 13:09:27 -0500

    Add Sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <BrowserRouter className="App">
+      <Sidebar />
       <Navbar />
     </BrowserRouter>
   );
