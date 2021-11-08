c71a6d16cc266f5098b9af4e7486f89eb05b8a03
Felix <34609664+z-felixhan@users.noreply.github.com>
2021-11-07 19:35:26 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-import Hero from "./components/Hero";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/login" element={<Login />} exact />
+      </Routes>
     </BrowserRouter>
   );
 }
