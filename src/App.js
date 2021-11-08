43627b6f47702e5e5e1308b325b503c4dc0133df
Felix <34609664+z-felixhan@users.noreply.github.com>
2021-11-08 16:27:00 -0500

    Use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <BrowserRouter className="App">
+    <HashRouter className="App">
       <Routes>
         <Route path="/" element={<Home />} exact />
         <Route path="/login" element={<Login />} exact />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
