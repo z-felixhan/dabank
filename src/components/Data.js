0ef5c0efd5296f998a49dfee062ef459fbebea91
Felix <34609664+z-felixhan@users.noreply.github.com>
2021-11-07 19:02:22 -0500

    Initial

---
+++ src/components/Data.js
@@ -0,0 +1,49 @@
+import aboutImage from "../images/undraw_personal_website_re_c8dv.svg";
+
+export const about = {
+  id: "about",
+  buttonLabel: "Get Started",
+  description: "Get access to our exclusive application!",
+  dark: true,
+  darkText: false,
+  headline: "Unlimited Transactions",
+  img: aboutImage,
+  imgStart: false,
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  primary: true,
+  topLine: "Premium Bank",
+};
+
+export const discover = {
+  id: "discover",
+  buttonLabel: "Get Started",
+  description: "Get access to our exclusive application!",
+  dark: true,
+  darkText: true,
+  headline: "Unlimited Transactions",
+  img: aboutImage,
+  imgStart: false,
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  primary: false,
+  topLine: "Premium Bank",
+};
+
+export const services = {
+  id: "services",
+  buttonLabel: "Get Started",
+  description: "Get access to our exclusive application!",
+  dark: true,
+  darkText: false,
+  headline: "Unlimited Transactions",
+  img: aboutImage,
+  imgStart: false,
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  primary: true,
+  topLine: "Premium Bank",
+};
