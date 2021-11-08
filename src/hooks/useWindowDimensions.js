92ee61eed75600a82b3cdfb333668f2c33475a04
Felix <34609664+z-felixhan@users.noreply.github.com>
2021-11-08 15:30:30 -0500

    Custom hook for screen resizing

---
+++ src/hooks/useWindowDimensions.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+function getWindowDimensions() {
+  const { innerWidth: innerWidth, innerHeight: innerHeight } = window;
+  return {
+    innerWidth,
+    innerHeight,
+  };
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
